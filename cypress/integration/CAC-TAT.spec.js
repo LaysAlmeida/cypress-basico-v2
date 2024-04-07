e529ba06ee9cdc56362bcd8988e4fd7054df0856
lays.almeida <lays.almeida@compasso.com.br>
2024-04-07 12:39:03 -0300

    add new tests

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -4,9 +4,11 @@ describe('Central de Atendimento ao Cliente TAT', function () {
         cy.visit('./src/index.html')
     })
     it('verifica o título da aplicação', function () {
-        cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT')
+        cy.title()
+            .should('be.equal', 'Central de Atendimento ao Cliente TAT')
     })
 
+    //Exercicio 1 e 2
     it('preenche os campos obrigatórios e envia o formulário', function () {
         cy.get('#firstName')
             .type('Bruno')
@@ -18,7 +20,7 @@ describe('Central de Atendimento ao Cliente TAT', function () {
             .type('emailteste@teste.com')
 
         cy.get('#open-text-area')
-            .type('Lorem ipsum dolor sit amet.')
+            .type('Lorem ipsum dolor sit amet, ipsum dolor sit amet, ipsum dolor sit amet, ipsum dolor sit amet, ipsum dolor sit amet.', { delay: 0 })
 
         cy.get('form .button')
             .click()
@@ -26,4 +28,84 @@ describe('Central de Atendimento ao Cliente TAT', function () {
         cy.get('.success').should('be.visible')
 
     })
+    
+    //Exercicio 3
+    it('exibe mensagem de erro ao submeter o formulário com um email com formatação inválida', function () {
+        cy.get('#firstName')
+            .type('Bruno')
+
+        cy.get('#lastName')
+            .type('Gomes')
+
+        cy.get('#email')
+            .type('emailteste.com')
+
+        cy.get('#open-text-area')
+            .type('Lorem ipsum dolor sit amet.')
+
+        cy.get('form .button')
+            .click()
+
+        cy.contains('Valide os campos obrigatórios!')
+
+    })
+
+    //Exercicio 4
+    it('validar valor vazio após inserir valores não-numericos no campo de telefone', function () {
+        cy.get('#firstName')
+            .type('Bruno')
+
+        cy.get('#lastName')
+            .type('Gomes')
+
+        cy.get('#email')
+            .type('email@teste.com')
+
+        cy.get('#phone')
+            .type('dsfsdfsdfsd')
+            .should('have.value', '')
+
+        cy.get('#open-text-area')
+            .type('Lorem ipsum dolor sit amet.')
+
+        cy.get('form .button')
+            .click()
+
+        cy.contains('Valide os campos obrigatórios!')
+
+    })
+
+    //Exercicio 5
+    it('exibe mensagem de erro quando o telefone se torna obrigatório mas não é preenchido antes do envio do formulário', function () {
+        cy.get('#firstName')
+            .type('Bruno')
+
+        cy.get('#lastName')
+            .type('Gomes')
+
+        cy.get('#email')
+            .type('emailteste@teste.com')
+        
+        cy.get('#phone-checkbox')
+            .click()
+
+        cy.get('#open-text-area')
+            .type('Lorem ipsum dolor sit amet, ipsum dolor sit amet, ipsum dolor sit amet, ipsum dolor sit amet, ipsum dolor sit amet.', {delay: 0})
+
+        cy.get('form .button')
+            .click()
+
+        cy.get('.error')
+            .should('be.visible')
+    })
+
+    //Exercício 6
+    it.only('exibe mensagem de erro ao submeter o formulário sem preencher os campos obrigatórios', function(){
+        cy.get('form .button')
+        .click()
+
+        cy.get('.error')
+        .should('be.visible')
+    })
+
 })
